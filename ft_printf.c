ccc719aee6d2509291bdeb59cf366cc821d5d418
gmonteiro13 <gscmonteiro@gmail.com>
2021-03-24 17:53:39 -0300

    Starting to work on pointer conversion

--- ft_printf.c
+++ ft_printf.c
@@ -19,6 +19,7 @@ int			ft_printf(const char *s, ...)
 	char	*printstring;
 	char	*integerstring;
 	int		printinteger;
+	void	*ptest;
 
 	/*
 	The return value is the number of characters printed, so we'll
@@ -84,6 +85,17 @@ int			ft_printf(const char *s, ...)
 					putchar(*integerstring++);
 				s++;
 			}
+			else if (*s == 'p')
+			/* Pointer conversion */
+			{
+				ptest = (char **)va_arg(list, char *);
+				printstring = ptest;
+				printf("%p", printstring);
+				charcount += strlen(printstring);
+				while (*printstring)
+					putchar(*printstring++);
+				s++;
+			}
 		}
 		else
 		{
@@ -110,8 +122,10 @@ int main()
 	test2 = 777;
 	printf("Original function:\n");
 	original_return = printf("Thank you, %s! But our princess is in castle %i.\n", test, test2);
+	printf("This is the test2 address %p\n", &test2);
 	printf("My function:\n");
 	my_return = ft_printf("Thank you, %s! But our princess is in castle %i.\n", test, test2);
+	ft_printf("This is test2 address %p\n", &test2);
 	printf("The original printf read %d characters!\n", original_return);
 	printf("My printf read %d characters!\n", my_return);
 }
